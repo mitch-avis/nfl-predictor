07907e904bcb1343cfad88ef8e00033d0587de20
mitch-avis <mitchavis@gmail.com>
2023-07-15 18:01:32 -0600

    clean up

--- app/ml_model.py
+++ app/ml_model.py
@@ -1,11 +1,39 @@
 #!/bin/env python
 
 import numpy as np
+import pandas as pd
 import xgboost as xgb
+from logger import log
 from sklearn.linear_model import LogisticRegression
+from utils.utils import display, get_dataframe
+
+NUM_ROUNDS = 1000
 
 
-def predict_games(pred_games_df, test_df, x_train, y_train, x_test, y_test, rounds):
+def main():
+    year = 2022
+    current_week = 18
+    comp_games_name = f"{year}_comp_games"
+    comp_games_df = get_dataframe(comp_games_name)
+    pred_games_name = f"{year}_{current_week}_pred_games"
+    pred_games_df = get_dataframe(pred_games_name)
+    predict_games(comp_games_df, pred_games_df)
+
+
+def predict_games(comp_games_df: pd.DataFrame, pred_games_df: pd.DataFrame) -> None:
+    msk = np.random.rand(len(comp_games_df)) < 0.8
+    train_df = comp_games_df[msk]
+    test_df = comp_games_df[~msk]
+
+    X_train = train_df.drop(
+        columns=["away_name", "away_abbr", "home_name", "home_abbr", "week", "result"]
+    )
+    y_train = train_df[["result"]]
+    X_test = test_df.drop(
+        columns=["away_name", "away_abbr", "home_name", "home_abbr", "week", "result"]
+    )
+    y_test = test_df[["result"]]
+
     clf = LogisticRegression(
         penalty="l1",
         dual=False,
@@ -20,14 +48,14 @@ def predict_games(pred_games_df, test_df, x_train, y_train, x_test, y_test, roun
         multi_class="ovr",
         verbose=0,
     )
-    clf.fit(x_train, np.ravel(y_train.values))
-    y_pred = clf.predict_proba(x_test)
-    y_pred = y_pred[:, 1]
-    display(y_pred, test_df)
-    # accuracy = accuracy_score(y_test, np.round(y_pred))
 
-    dtest = xgb.DMatrix(x_test, y_test, feature_names=x_test.columns)
-    dtrain = xgb.DMatrix(x_train, y_train, feature_names=x_train.columns)
+    clf.fit(X_train, np.ravel(y_train.values))
+    y_pred = clf.predict_proba(X_test)
+    y_pred = y_pred[:, 1]
+
+    display(y_pred, test_df)
+    dtest = xgb.DMatrix(X_test, y_test, feature_names=X_test.columns)
+    dtrain = xgb.DMatrix(X_train, y_train, feature_names=X_train.columns)
     param = {
         "booster": "gblinear",
         "verbosity": 1,
@@ -38,16 +66,13 @@ def predict_games(pred_games_df, test_df, x_train, y_train, x_test, y_test, roun
         "eval_metric": "error",
     }
     watchlist = [(dtest, "eval"), (dtrain, "train")]
-    bst = xgb.train(param, dtrain, rounds, watchlist)
+    bst = xgb.train(param, dtrain, NUM_ROUNDS, watchlist)
     preds = bst.predict(dtest)
     labels = dtest.get_label()
-    print(
-        "error=%f"
-        % (
-            sum(1 for i in range(len(preds)) if int(preds[i] > 0.5) != labels[i])
-            / float(len(preds))
-        )
+    error = sum(1 for i in range(len(preds)) if int(preds[i] > 0.5) != labels[i]) / float(
+        len(preds)
     )
+    log.debug(f"error={error}")
     X_test = pred_games_df.drop(
         columns=["away_name", "away_abbr", "home_name", "home_abbr", "week", "result"]
     )
@@ -55,22 +80,6 @@ def predict_games(pred_games_df, test_df, x_train, y_train, x_test, y_test, roun
     y_pred = y_pred[:, 1]
 
     display(y_pred, pred_games_df)
-
-
-def display(y_pred, x_test):
-    for game in range(len(y_pred)):
-        win_prob = round(y_pred[game] * 100, 2)
-        week = x_test.reset_index().drop(columns="index").loc[game, "week"]
-        away_team = x_test.reset_index().drop(columns="index").loc[game, "away_name"]
-        home_team = x_test.reset_index().drop(columns="index").loc[game, "home_name"]
-        print(
-            f"Week {week}: The {away_team} had a probability of {win_prob}% of beating the "
-            f"{home_team}."
-        )
-
-
-def main():
-    return
 
 
 if __name__ == "__main__":

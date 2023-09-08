b8e4b9c58b3f221e546b089476566819e12bac3b
mitch-avis <mitchavis@gmail.com>
2023-09-08 16:09:19 -0600

    move logger config

--- src/definitions.py
+++ src/definitions.py
@@ -2,35 +2,6 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(ROOT_DIR, "data")
-LOGGING_CONFIG = {
-    "version": 1,
-    "disable_existing_loggers": True,
-    "formatters": {
-        "default": {
-            "format": (
-                "[%(asctime)s.%(msecs)03d][%(levelname)s]"
-                "[%(name)s:%(filename)s:%(funcName)s:%(lineno)s][%(threadName)s] %(message)s"
-            ),
-            "datefmt": "%Y-%m-%d %H:%M:%S",
-            "class": "coloredlogs.ColoredFormatter",
-        },
-    },
-    "handlers": {
-        "default": {
-            "level": "DEBUG",
-            "formatter": "default",
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stderr",
-        },
-    },
-    "loggers": {
-        "root": {
-            "handlers": ["default"],
-            "level": "DEBUG",
-            "propagate": False,
-        },
-    },
-}
 ELO_DATA_URL = "https://projects.fivethirtyeight.com/nfl-api/nfl_elo.csv"
 AWAY_STATS = [
     "away_first_downs",
--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,7 +1,35 @@
 import logging
 from logging.config import dictConfig
 
-from definitions import LOGGING_CONFIG
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": {
+        "default": {
+            "format": (
+                "[%(asctime)s.%(msecs)03d][%(levelname)s]"
+                "[%(name)s:%(filename)s:%(funcName)s:%(lineno)s][%(threadName)s] %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+            "class": "coloredlogs.ColoredFormatter",
+        },
+    },
+    "handlers": {
+        "default": {
+            "level": "DEBUG",
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        },
+    },
+    "loggers": {
+        "root": {
+            "handlers": ["default"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+    },
+}
 
 dictConfig(LOGGING_CONFIG)
 log = logging.getLogger()

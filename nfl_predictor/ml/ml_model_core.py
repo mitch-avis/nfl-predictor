b32194d50e307a7865c0ea28a1d84a4d6b676c0d
Mitch Avis <mitchavis@gmail.com>
2026-01-16 23:27:07 -0700

    style: move import of __main__ to correct location

--- nfl_predictor/ml/ml_model_core.py
+++ nfl_predictor/ml/ml_model_core.py
@@ -16,7 +16,6 @@ from dataclasses import dataclass, replace
 from pathlib import Path
 from typing import Any, Iterable, Optional, Sequence
 
-import __main__
 import joblib
 import numpy as np
 import optuna
@@ -37,6 +36,7 @@ from sklearn.metrics import (
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
+import __main__
 from nfl_predictor import constants
 from nfl_predictor.ml.ml_model_xgb_utils import (
     _build_xgb_fit_kwargs,
